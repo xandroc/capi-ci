15e1b51964bf80570d2437abfe4152ae7c4d4256
Utako Ueda <uueda@pivotal.io>
2016-03-30 16:13:34 -0700

    echo dependencies to get to the bottom of weird gemnasium api behavior

--- ci/scripts/cloud_controller_ng_dependencies.sh
+++ ci/scripts/cloud_controller_ng_dependencies.sh
@@ -3,22 +3,26 @@
 set -e
 
 cd cloud_controller_ng
+
+echo "retrieving dependency list"
 gemnasium --token $GEMNASIUM_API_KEY dependency_files push -f=Gemfile.lock
 dependencies=$(gemnasium --token $GEMNASIUM_API_KEY dependencies list)
-red_dependecies=$(echo -en "${dependencies}" | grep "red" | cut -f 2 -d '|' | sed 's/+-- //g')
-yellow_dependecies=$(echo -en "${dependencies}" | grep "yellow" | cut -f 2 -d '|' | sed 's/+-- //g')
+echo $dependencies
+
+red_dependencies=$(echo -en "${dependencies}" | grep "red" | cut -f 2 -d '|' | sed 's/+-- //g')
+yellow_dependencies=$(echo -en "${dependencies}" | grep "yellow" | cut -f 2 -d '|' | sed 's/+-- //g')
 
 echo
 echo
 echo "YELLOW DEPENDENCIES:"
 echo "--------------------"
-echo -e "${yellow_dependecies}"
+echo -e "${yellow_dependencies}"
 
-if [[ "${red_dependecies}" != "" ]]; then
+if [[ "${red_dependencies}" != "" ]]; then
   echo
   echo
   echo "RED DEPENDENCIES:"
   echo "--------------------"
-  echo -e "${red_dependecies}"
+  echo -e "${red_dependencies}"
   exit 1
 fi
