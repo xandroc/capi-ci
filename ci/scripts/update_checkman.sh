0b16b43518a6a1407f4652e27c5922e70f5bf3ed
Urvashi Reddy <ureddy@pivotal.io>
2016-04-06 14:05:11 -0700

    Add script for updating checkman
    [#116650623]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

---
+++ ci/scripts/update_checkman.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -ex
+
+atc_url=https://capi.ci.cf-app.com
+
+pushd capi-checkman
+  rm pipeline
+
+  pipelines=$(curl $atc_url/api/v1/pipelines -s | jq .[].name --raw-output)
+  for pipeline in $pipelines; do
+    curl -s "https://capi.ci.cf-app.com/api/v1/pipelines/$pipeline/jobs" | jq .[].name --raw-output | awk -v pipeline="$pipeline" -v atc_url="$atc_url" '{print $1 ": concourse.check " atc_url " " pipeline " " $1}' >> pipeline
+  done
+
+  set +e
+  git diff-index --quiet HEAD --
+  exit_code=$?
+  set -e
+
+  if [[ $exit_code -ne 0 ]]; then
+    git config user.name "CAPI CI"
+    git config user.email "cf-capi-eng+ci@pivotal.io"
+
+    git add pipeline
+    git commit -m "Update pipeline jobs"
+  fi
+popd
+
+cp -r capi-checkman/. updated-capi-checkman
