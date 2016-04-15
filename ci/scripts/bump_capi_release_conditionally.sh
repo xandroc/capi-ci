35c3e0cc7740536ac2d12d58a5a7e98f1e35b2e8
Peter Goetz <peter.gtz@gmail.com>
2016-04-15 14:42:01 -0700

    Block bump-capi-release job on migrations job
    [#115978473]
    
    Signed-off-by: Kara Felix <kfelix@us.ibm.com>

--- ci/scripts/bump_capi_release_conditionally.sh
+++ ci/scripts/bump_capi_release_conditionally.sh
@@ -3,12 +3,23 @@ set -e -x
 
 source ~/.bashrc
 
-pushd $SOURCE_REPO
+pushd cloud_controller_ng
   SOURCE_MASTER_SHA=$(git rev-parse HEAD)
+  PASSED_UNIT_TESTS_SHA=$(git log -n1 --format="%H" -- db)
 popd
 
+pushd cloud_controller_ng-master-migrations
+  PASSED_DB_MIGRATIONS_SHA=$(git log -n1 --format="%H" -- db)
+popd
+
+echo "Checking if db folder from unit tests is same as db folder from migration tests..."
+if [[ $PASSED_UNIT_TESTS_SHA != $PASSED_DB_MIGRATIONS_SHA ]]; then
+  echo "The commits do not match; waiting for alignment. Not bumping."
+  exit 0
+fi
+
 pushd capi-release
-  pushd "$DESTINATION_SUBMODULE"
+  pushd src/cloud_controller_ng
     git fetch
     git checkout $SOURCE_MASTER_SHA
   popd
@@ -25,7 +36,7 @@ pushd capi-release
     git config user.name "CAPI CI"
     git config user.email "cf-capi-eng+ci@pivotal.io"
 
-    git add "$DESTINATION_SUBMODULE"
+    git add src/cloud_controller_ng
 
     scripts/staged_shortlog
     scripts/staged_shortlog | git commit -F -
