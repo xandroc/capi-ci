aff3048fdc623a931193ea8298670c33429a5989
Kara Alexandra <kalexandra@us.ibm.com>
2016-04-21 15:08:15 -0700

    bump-capi-release stays green, does not bump when SHAs do not match
    Signed-off-by: Eric Saxby <esaxby@pivotal.io>

--- ci/scripts/bump_capi_release_conditionally.sh
+++ ci/scripts/bump_capi_release_conditionally.sh
@@ -13,34 +13,33 @@ pushd cloud_controller_ng-master-migrations
 popd
 
 echo "Checking if db folder from unit tests is same as db folder from migration tests..."
-if [[ $PASSED_UNIT_TESTS_SHA != $PASSED_DB_MIGRATIONS_SHA ]]; then
-  echo "The commits do not match; waiting for alignment. Not bumping."
-  exit 0
-fi
+if [[ $PASSED_UNIT_TESTS_SHA == $PASSED_DB_MIGRATIONS_SHA ]]; then
+  pushd capi-release
+    pushd src/cloud_controller_ng
+      git fetch
+      git checkout $SOURCE_MASTER_SHA
+    popd
 
-pushd capi-release
-  pushd src/cloud_controller_ng
-    git fetch
-    git checkout $SOURCE_MASTER_SHA
+    set +e
+      git diff --exit-code
+      exit_code=$?
+    set -e
+
+    if [[ $exit_code -eq 0 ]]
+    then
+      echo "There are no changes to commit."
+    else
+      git config user.name "CAPI CI"
+      git config user.email "cf-capi-eng+ci@pivotal.io"
+
+      git add src/cloud_controller_ng
+
+      scripts/staged_shortlog
+      scripts/staged_shortlog | git commit -F -
+    fi
   popd
-
-  set +e
-    git diff --exit-code
-    exit_code=$?
-  set -e
-
-  if [[ $exit_code -eq 0 ]]
-  then
-    echo "There are no changes to commit."
-  else
-    git config user.name "CAPI CI"
-    git config user.email "cf-capi-eng+ci@pivotal.io"
-
-    git add src/cloud_controller_ng
-
-    scripts/staged_shortlog
-    scripts/staged_shortlog | git commit -F -
-  fi
-popd
+else
+  echo "The commits do not match; waiting for alignment. Not bumping."
+fi
 
 cp -r capi-release bumped/capi-release
