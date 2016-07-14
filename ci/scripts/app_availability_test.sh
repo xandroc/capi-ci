35ad624ad1135258636685b05528beeca9470cd5
Urvashi Reddy <ureddy@pivotal.io>
2016-07-14 11:25:39 -0700

    give this release a name
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- ci/scripts/app_availability_test.sh
+++ ci/scripts/app_availability_test.sh
@@ -37,7 +37,7 @@ function deploy_migrate_and_kill() {
     bosh target "${bosh_target}"
     bosh login "${bosh_lite_username}" "${bosh_lite_password}"
 
-    bosh create release cf --force
+    bosh create release --force | tee -a cf
     EXIT_STATUS=${PIPESTATUS[0]}
     if [ ! "$EXIT_STATUS" = "0" ]; then
       echo "Failed to create cf release"
