570bccd5276ba45eaf235df2a56a1621f7c62fa0
Utako Ueda <uueda@pivotal.io>
2016-07-14 11:36:21 -0700

    non interactive create release please
    Signed-off-by: Urvashi Reddy <ureddy@pivotal.io>

--- ci/scripts/app_availability_test.sh
+++ ci/scripts/app_availability_test.sh
@@ -37,7 +37,7 @@ function deploy_migrate_and_kill() {
     bosh target "${bosh_target}"
     bosh login "${bosh_lite_username}" "${bosh_lite_password}"
 
-    bosh create release --force | tee -a cf
+    bosh -n create release --force
     EXIT_STATUS=${PIPESTATUS[0]}
     if [ ! "$EXIT_STATUS" = "0" ]; then
       echo "Failed to create cf release"
