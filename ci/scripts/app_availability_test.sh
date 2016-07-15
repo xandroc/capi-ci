10dd9ed5ab7af244dd91cbcddbadd971f5056520
Utako Ueda <uueda@pivotal.io>
2016-07-14 18:21:45 -0700

    update app availability connection string

--- ci/scripts/app_availability_test.sh
+++ ci/scripts/app_availability_test.sh
@@ -42,7 +42,8 @@ function deploy_migrate_and_kill() {
       ${TUNNEL_HOST} \
       'bash -c "export local_ip=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4` && sudo iptables -t nat -A PREROUTING -p tcp -d \$local_ip --dport 9000 -j DNAT --to 10.244.0.30:5524"'
 
-    DB_CONNECTION_STRING="${CONNECTION_STRING}" bundle exec rake db:migrate
+    export DB_CONNECTION_STRING="${CONNECTION_STRING}"
+    bundle exec rake db:migrate
   popd
 
   # wait for nsync bulker to poll and bbs to potentially kill running app instances
