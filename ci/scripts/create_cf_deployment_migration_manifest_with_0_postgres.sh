f52a2cc795b3e8b217973b309ee920949069b3e3
Simon Leung <leungs@us.ibm.com>
2016-08-11 11:26:08 -0700

    Scale and reset postgres vm
    [#128120047]
    
    Signed-off-by: Amelia Downs <adowns@pivotal.io>

---
+++ ci/scripts/create_cf_deployment_migration_manifest_with_0_postgres.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+source ~/.bashrc
+
+pushd cf-release/src/capi-release/src/cloud_controller_ng
+  git checkout migrate
+  git pull
+popd
+
+cd cf-release
+
+./scripts/generate_deployment_manifest ${INFRASTRUCTURE} ../capi-ci/${ENVIRONMENT}/stubs/cf/with-0-postgres/*.yml ../capi-ci-private/${ENVIRONMENT}/stubs/cf/*.yml > ../generated-manifest/deployment.yml
+
+echo "===GENERATED MANIFEST==="
+cat ../generated-manifest/deployment_with_0_postgres.yml
+
+
