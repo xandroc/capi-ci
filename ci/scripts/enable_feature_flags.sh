7e9cf4afd4e30f6c7b13cff510ecc1989f447296
Amelia Downs <adowns@pivotal.io>
2016-08-16 14:39:34 -0700

    enable feature flags before cats on arya
    [#128345063]
    
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

---
+++ ci/scripts/enable_feature_flags.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e -x
+
+source ~/.bashrc
+
+cf api api.arya.cf-app.com
+cf auth admin ${CF_ADMIN_PASSWORD}
+cf enable-feature-flag diego_docker
+cf enable-feature-flag task_creation
+cf enable-feature-flag env_var_visibility
+
+bosh -n --color -t $BOSH_DIRECTOR -d $DEPLOYMENT_YML $COMMAND
+
