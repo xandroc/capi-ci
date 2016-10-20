86747974234bf8ca7ed734151110451bc563936e
Zach Robinson <zrobinson@pivotal.io>
2016-10-20 14:24:07 -0700

    set verbosity
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- ci/scripts/enable_feature_flags.sh
+++ ci/scripts/enable_feature_flags.sh
@@ -4,6 +4,10 @@ set -e -x
 source ~/.bashrc
 
 cf api ${CF_API_TARGET} --skip-ssl-validation
+
+set +x
 cf auth admin ${CF_ADMIN_PASSWORD}
+set -x
+
 cf enable-feature-flag diego_docker
 cf enable-feature-flag task_creation
