b6dc7dbf00755e619533fb4f7ab4f886c9b3f078
Zach Robinson <zrobinson@pivotal.io>
2016-11-09 16:27:48 -0800

    reorganize
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

---
+++ ci/scripts/merge_master_into_develop.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e -x -u
+
+pushd capi-release
+  echo "----- Set git identity"
+  git config user.name "CAPI CI"
+  git config user.email "cf-capi-eng+ci@pivotal.io"
+
+  echo "----- Adding master cloned release as remote"
+  git remote add local-capi-release-master ../capi-release-master
+  git fetch local-capi-release-master
+  git merge --no-edit local-capi-release-master/master
+popd
+
+cp -a capi-release merged/capi-release
