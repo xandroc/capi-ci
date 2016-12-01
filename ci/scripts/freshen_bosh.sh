1b1274f40ea78bc90fa8607e7a607dfd3866c42e
Jonathan Berkhahn <jaberkha@us.ibm.com>
2016-12-01 10:50:01 -0800

    update freshen_bosh scripts
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

---
+++ ci/scripts/freshen_bosh.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -xeu
+DIR="$(pwd)"
+
+function commit_bbl_state_file {
+  pushd "${DIR}/env-repo"
+    if [[ -n $(git status --porcelain) ]]; then
+      git config user.name "CAPI CI"
+      git config user.email "cf-capi-eng+ci@pivotal.io"
+      git add "$BBL_DIR"
+      git commit -m "Update bbl artifacts"
+    fi
+  popd
+
+  pushd "${DIR}"
+    shopt -s dotglob
+    cp -R env-repo/* updated-env-repo/
+  popd
+}
+
+trap commit_bbl_state_file EXIT
+
+pushd "env-repo/${BBL_DIR}"
+  set +x
+  echo "source .envrc"
+  source .envrc
+  set -x
+
+  bbl up --aws-region us-east-1
+
+  # The two commands below amount to "create or update"
+  bbl \
+    create-lbs \
+    --type=cf \
+    --cert=certs/load-balancer/*.crt \
+    --key=certs/load-balancer/*.key \
+    --skip-if-exists
+
+  bbl \
+    update-lbs \
+    --cert=certs/load-balancer/*.crt \
+    --key=certs/load-balancer/*.key \
+popd
