75100139e8a1eeb3113a88dac85f569325b8bee5
Chunyi Lyu <clyu@pivotal.io>
2017-02-14 10:06:38 -0800

    Support uploading custom release for cf-deployment
    * capi-release has symlinks, so we cannot use Rel-Ints concourse tasks
    to construct the release for us without having them add all of our
    pre-packaging dependencies.
    * That also means we have to bring over our ops files until they can
    support them being in two different repositories.
    
    [#133804829]
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

---
+++ ci/scripts/upload-release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eux
+
+function setup_bosh_env_vars() {
+  set +x
+  pushd "bbl-state/${BBL_STATE_DIR}"
+    export BOSH_CA_CERT="$(bbl director-ca-cert)"
+    export BOSH_ENVIRONMENT=$(bbl director-address)
+    export BOSH_CLIENT=$(bbl director-username)
+    export BOSH_CLIENT_SECRET=$(bbl director-password)
+  popd
+  set -x
+}
+
+function upload_release() {
+  for filename in "release-tarball/*.tgz"; do
+    bosh upload-release "$filename"
+  done
+}
+
+function main() {
+  setup_bosh_env_vars
+  upload_release
+}
+
+main
