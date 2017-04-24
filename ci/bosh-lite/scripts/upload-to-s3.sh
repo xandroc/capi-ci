f385eeacf773ea4e177a4cd4a43fcdb71a2f118b
Jen Spinney <jennifer.spinney@suse.com>
2017-04-24 16:33:04 -0700

    Fix incorrect path when S3_PATH is omitted
    [#143300605]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/bosh-lite/scripts/upload-to-s3.sh
+++ ci/bosh-lite/scripts/upload-to-s3.sh
@@ -15,11 +15,15 @@ source_dir="${workspace_dir}/source-directory"
 : ${S3_BUCKET:?}
 
 : ${S3_PATH:=""}
-: ${AWS_DEFAULT_REGION:=""}
+: ${AWS_DEFAULT_REGION:=us-east-1}
 
 # TASK
 pushd "${source_dir}" > /dev/null
   for filename in ${FILE_NAMES}; do
-    aws --endpoint-url "${AWS_ENDPOINT}" s3 cp "$filename" s3://${S3_BUCKET}/${S3_PATH}/
+    remote_path="s3://${S3_BUCKET}/"
+    if [ -n "${S3_PATH}" ]; then
+      remote_path="${remote_path}/${S3_PATH}/"
+    fi
+    aws --endpoint-url "${AWS_ENDPOINT}" s3 cp "$filename" "${remote_path}"
   done
 popd > /dev/null
