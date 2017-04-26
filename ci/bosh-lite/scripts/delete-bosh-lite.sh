c002dde302cd60a594890805e328b9f1fbe47396
Matt Royal <mroyal@pivotal.io>
2017-04-25 17:51:20 -0700

    Simplify delete job by saving director manifest
    [#143300605]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/bosh-lite/scripts/delete-bosh-lite.sh
+++ ci/bosh-lite/scripts/delete-bosh-lite.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-# ENV
-: "${GCP_JSON_KEY:?}"
-
 # INPUTS
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../../" && pwd )"
@@ -13,23 +10,11 @@ terraform_dir="${workspace_dir}/terraform"
 state_dir="${workspace_dir}/director-state"
 
 pushd "${state_dir}" > /dev/null
-  bosh interpolate "${deployment_repo}/bosh.yml" \
-    -o "${deployment_repo}/gcp/cpi.yml" \
-    -o "${deployment_repo}/bosh-lite.yml" \
-    -o "${deployment_repo}/bosh-lite-runc.yml" \
-    -o "${deployment_repo}/gcp/bosh-lite-vm-type.yml" \
-    -o "${deployment_repo}/jumpbox-user.yml" \
-    -o "${deployment_repo}/external-ip-not-recommended.yml" \
-    -v director_name="Bosh Lite Director" \
-    -v gcp_credentials_json="'${GCP_JSON_KEY}'" \
-    -l "${terraform_dir}/metadata" \
-    > ./director.yml
+  echo -e "\nDestroying the Bosh-Lite..."
+  bosh delete-env \
+     --state ./state.json \
+     --vars-store ./creds.yml \
+    ./director.yml
 
-    echo -e "\nDestroying the Bosh-Lite..."
-    bosh delete-env \
-       --state ./state.json \
-       --vars-store ./creds.yml \
-      ./director.yml
-
-    echo "Done"
+  echo "Done"
 popd > /dev/null
