88c60e31d20266e6ef11c04d657cfb8a555cf197
Lyle Franklin <lfranklin@pivotal.io>
2017-05-11 14:21:00 -0700

    Add task to create kiki dns record
    - Will move other envs over if it works
    
    [#145236545]
    
    Signed-off-by: Matt Royal <mroyal@pivotal.io>

---
+++ ci/bbl-tasks/create-dns-record.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu
+
+# ENV
+: "${BBL_STATE_DIR:?}"
+: "${DNS_DOMAIN:?}"
+: "${SHARED_DNS_ZONE_NAME:?}"
+: "${GCP_DNS_SERVICE_ACCOUNT_KEY:?}"
+
+# INPUTS
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
+bbl_state_dir="${workspace_dir}/bbl-state/${BBL_STATE_DIR}"
+
+create_dns_record() {
+  gcloud auth activate-service-account --key-file=<( echo "${GCP_DNS_SERVICE_ACCOUNT_KEY}" )
+
+  record_count="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json | jq 'length' )"
+  if [ "${record_count}" == "0" ]; then
+    bbl_name_servers="$( bbl lbs --json | jq -r '.cf_system_domain_dns_servers | join(\" \")' )"
+
+    gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
+    gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${bbl_name_servers}
+    gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
+  fi
+}
+
+pushd "${bbl_state_dir}" > /dev/null
+  create_dns_record
+popd > /dev/null
