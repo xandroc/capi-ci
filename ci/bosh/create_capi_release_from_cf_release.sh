a31fd3d2093a30e55791e1a5c504667092e2f06d
Lyle Franklin <lfranklin@pivotal.io>
2017-05-19 15:19:06 -0700

    [kiki] Build and upload capi-release from cf-release
    - We'd like to catch migration bugs from latest cf-release to current
      develop of CAPI
    
    [#139962597]
    
    Signed-off-by: Jen Spinney <jennifer.spinney@suse.com>

---
+++ ci/bosh/create_capi_release_from_cf_release.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+set -eu
+
+# ENV
+
+# INPUTS
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd "${script_dir}/../../.." && pwd )"
+bbl_vars_file="${workspace_dir}/environment/metadata"
+cf_release_dir="${workspace_dir}/cf-release"
+
+# TODO: extract into util script
+BOSH_ENVIRONMENT="$(jq -e -r .target "${bbl_vars_file}")"
+BOSH_CLIENT="$(jq -e -r .client "${bbl_vars_file}")"
+BOSH_CLIENT_SECRET="$(jq -e -r .client_secret "${bbl_vars_file}")"
+BOSH_CA_CERT="$(jq -e -r .ca_cert "${bbl_vars_file}")"
+BOSH_GW_USER="$(jq -e -r .gw_user "${bbl_vars_file}")"
+BOSH_GW_HOST="$(jq -e -r .gw_host "${bbl_vars_file}")"
+BOSH_GW_PRIVATE_KEY_CONTENTS="$(jq -e -r .gw_private_key "${bbl_vars_file}")"
+export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT \
+  BOSH_GW_USER BOSH_GW_HOST BOSH_GW_PRIVATE_KEY_CONTENTS
+
+pushd "${cf_release_dir}/src/capi-release" > /dev/null
+  # TODO: switch to Golang bosh once it supports symlinks
+  bosh1 -n --parallel 10 create release
+
+  bosh -n upload-release --rebase
+popd > /dev/null
