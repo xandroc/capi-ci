a0d2c4459a3e556ba8e6a757e9a34a8ca86fb4bb
Lyle Franklin <lfranklin@pivotal.io>
2017-05-19 10:52:44 -0700

    Create DNS record even if bbl changes nameservers
    - Previous implementation would not update an existing record
    
    Signed-off-by: Jen Spinney <jennifer.spinney@suse.com>

--- ci/bbl-tasks/create-dns-record.sh
+++ ci/bbl-tasks/create-dns-record.sh
@@ -18,14 +18,20 @@ create_dns_record() {
   gcloud auth activate-service-account --key-file=<( echo "${GCP_DNS_SERVICE_ACCOUNT_KEY}" )
   gcloud config set project "${GCP_PROJECT_ID}"
 
-  record_count="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json | jq 'length' )"
-  if [ "${record_count}" == "0" ]; then
-    bbl_name_servers="$( bbl lbs --json | jq -r '.cf_system_domain_dns_servers | join(" ")' )"
+  bbl_name_servers_json="$( bbl lbs --json | jq -r '.cf_system_domain_dns_servers' )"
+  bbl_name_servers_raw="$( echo "${bbl_name_servers_json}" | jq -r 'join(" ")' )"
+  gcp_name_servers_json="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json )"
 
-    gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
-    gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${bbl_name_servers}
-    gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
+  gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
+
+  record_count="$( echo "${gcp_name_servers_json}" | jq 'length' )"
+  if [ "${record_count}" != "0" ]; then
+    gcp_name_servers_raw="$( echo "${gcp_name_servers_json}" | jq -r '.[0].rrdatas | join(" ")' )"
+    gcloud dns record-sets transaction remove --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${gcp_name_servers_raw}
   fi
+
+  gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${bbl_name_servers_raw}
+  gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
 }
 
 pushd "${bbl_state_dir}" > /dev/null
