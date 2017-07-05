675ec61a797fd475ef7aee0d88fc9d0af91c8a00
Tim Downey <tdowney@pivotal.io>
2017-07-05 15:01:32 -0700

    Update delete-bosh-lite task to continue when deletion fails
    - Previously the task would fail when run more than once since deletion
    may have already occurred.  This change will make the task a bit more
    idempotent and we expect the following terraform job to fail if there
    are any stragglers
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/bosh-lite/delete-bosh-lite.sh
+++ ci/bosh-lite/delete-bosh-lite.sh
@@ -9,10 +9,17 @@ state_dir="${workspace_dir}/director-state"
 
 pushd "${state_dir}" > /dev/null
   echo -e "\nDestroying the Bosh-Lite..."
+  set +e
   bosh delete-env \
      --state ./state.json \
      --vars-store ./creds.yml \
     ./director.yml
+  exit_code="$?"
+  set -e
+
+  if [ "${exit_code}" != "0" ]; then
+    echo "Failed to delete bosh-lite. Continuing as it may have been deleted out of band..."
+  fi
 
   echo "Done"
 popd > /dev/null
