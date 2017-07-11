16fa2888436cd63e06c08163c94166c75a6b42ab
Eric Promislow <eric.promislow@gmail.com>
2017-07-11 10:52:33 -0700

    Temporarily use a release-candidate stemcell for windows-cell
    Signed-off-by: Annie Sing <asing@pivotal.io>

--- ci/bosh/upload-windows-stemcell-for-gcp.sh
+++ ci/bosh/upload-windows-stemcell-for-gcp.sh
@@ -11,7 +11,8 @@ function setup_bosh_env_vars() {
 }
 
 function upload_stemcell() {
-  bosh upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-windows2012R2-go_agent
+  # bosh upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-windows2012R2-go_agent
+  bosh upload-stemcell 'https://s3.amazonaws.com/bosh-windows-stemcells-release-candidates/light-bosh-stemcell-1093.0.0-build.1-google-kvm-windows2012R2-go_agent.tgz'
 }
 
 function main() {
