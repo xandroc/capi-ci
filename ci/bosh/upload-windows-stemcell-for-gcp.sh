65a57b48807d77424ed5499c2324e282abdbac40
Jen Spinney <jennifer.spinney@suse.com>
2017-07-20 11:23:22 -0700

    Use latest windows GCP stemcell
    * We were locked to a particular version (1093) because of a bug, but now the
    latest version should be fine.
    
    [finishes #148771687]

--- ci/bosh/upload-windows-stemcell-for-gcp.sh
+++ ci/bosh/upload-windows-stemcell-for-gcp.sh
@@ -11,8 +11,7 @@ function setup_bosh_env_vars() {
 }
 
 function upload_stemcell() {
-  # bosh upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-windows2012R2-go_agent
-  bosh upload-stemcell 'https://s3.amazonaws.com/bosh-windows-stemcells-release-candidates/light-bosh-stemcell-1093.0.0-build.1-google-kvm-windows2012R2-go_agent.tgz'
+  bosh upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-windows2012R2-go_agent
 }
 
 function main() {
