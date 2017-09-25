0470c6e602987e9c597ffe5bdff664a0500af9f6
Raina Masand <rainmaker@pivotal.io>
2017-09-25 10:15:18 -0700

    Add logging and refactors to run-drats script
    [#149590307]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/drats/run-drats.sh
+++ ci/drats/run-drats.sh
@@ -11,7 +11,13 @@ mkdir -p /home/jumpbox/golang/go/src/github.com
 mkdir -p /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/
 apt-get install -y git
 
-cd /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator && git clone https://github.com/tcdowney/disaster-recovery-acceptance-tests.git && cd disaster-recovery-acceptance-tests && git checkout add-cf-admin-password-hack
+echo "Downloading drats"
+if [[ ! -d /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator ]]; then
+  git clone https://github.com/tcdowney/disaster-recovery-acceptance-tests.git /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests
+  pushd /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests
+    git checkout add-cf-admin-password-hack
+  popd
+fi
 
 export DEPLOYMENT_TO_BACKUP=cf
 export DEPLOYMENT_TO_RESTORE=cf
@@ -25,26 +31,51 @@ export GOPATH=/home/jumpbox/golang/go
 export PATH=$PATH:$GOPATH/bin
 export TMPDIR=/home/jumpbox/tmp
 
-wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz -P /tmp && \
-  tar xzvf /tmp/go1.9.linux-amd64.tar.gz -C /usr/local && \
-  mkdir -p $GOPATH && \
-  rm -rf /tmp/*
-export PATH=$PATH:/usr/local/go/bin
+echo "Installing golang"
+TMPGO=/tmp/golang-temp
+if [[ ! -d ${TMPGO} ]]; then 
+  mkdir ${TMPGO}
+fi
 
+pushd ${TMPGO}
+  wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz -P ${TMPGO}
+  tar xzvf ${TMPGO}/go1.9.linux-amd64.tar.gz -C /usr/local
+  mkdir -p $GOPATH
+  export PATH=$PATH:/usr/local/go/bin
+popd
+
+rm -rf ${TMPGO}
+
+echo "Installing https transport package"
 sudo apt-get install -y --force-yes apt-transport-https
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 # ...then, update your local package index, then finally install the cf CLI
 sudo apt-get update -y
+
+echo "Installing cf cli"
 sudo apt-get install cf-cli -y
 
+echo "Installing glide"
 sudo add-apt-repository ppa:masterminds/glide -y && sudo apt-get update -y
 sudo apt-get install glide -y
 
-(cd $TMPDIR && wget https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.31-linux-amd64 -O bosh-cli && chmod +x bosh-cli && mv bosh-cli /usr/local/bin/bosh-cli)
-cd /home/jumpbox/bbr
-wget https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.0.0/bbr-1.0.0.tar
-tar -xf bbr-1.0.0.tar
-chmod a+x releases/bbr
+echo "Installing bosh cli"
+pushd $TMPDIR && 
+  wget https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.31-linux-amd64 -O bosh-cli
+  chmod +x bosh-cli
+  mv bosh-cli /usr/local/bin/bosh-cli
+popd
 
-/home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests/scripts/run_acceptance_tests.sh
+echo "Fetching BBR"
+
+pushd /home/jumpbox/bbr
+  wget https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.0.0/bbr-1.0.0.tar
+  tar -xf bbr-1.0.0.tar
+  chmod a+x releases/bbr
+popd
+
+echo "Fetching BBR"
+pushd /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests
+  /home/jumpbox/golang/go/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests/scripts/run_acceptance_tests.sh
+popd
