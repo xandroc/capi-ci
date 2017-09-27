53aa4dd670a915db41483a69c760b0c67cf1fe51
Elena Sharma <esharma@pivotal.io>
2017-09-26 18:02:30 -0700

    Fix typo in run-drats
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/drats/run-drats.sh
+++ ci/drats/run-drats.sh
@@ -18,7 +18,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 drats_dir="${workspace_dir}/drats"
 
-tmp_dir="$( mktemp -d /tmp/run-drats.XXXXXXXXXX )"
+tmpdir="$( mktemp -d /tmp/run-drats.XXXXXXXXXX )"
 
 ssh_key="${tmpdir}/bosh.pem"
 echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${ssh_key}"
@@ -28,7 +28,7 @@ tunnel_pid="$!"
 
 cleanup() {
   kill "${tunnel_pid}"
-  rm -rf "${tmp_dir}"
+  rm -rf "${tmpdir}"
 }
 trap 'cleanup' EXIT
 
