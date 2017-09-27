fd224594184cbb3cebaaf5e4a5e2fba4e4313380
Greg Cobb <gcobb@pivotal.io>
2017-09-27 15:16:16 -0700

    Add first pass at deploy bosh-lite to xena
    [#149590307]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- ci/bbl-tasks/create-dns-record.sh
+++ ci/bbl-tasks/create-dns-record.sh
@@ -8,6 +8,7 @@ set -eu -o pipefail
 : "${SHARED_DNS_ZONE_NAME:?}"
 : "${GCP_DNS_SERVICE_ACCOUNT_KEY:?}"
 : "${GCP_PROJECT_ID:?}"
+: "${IS_BOSH_LITE:=false}"
 
 # INPUTS
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -23,19 +24,34 @@ create_dns_record() {
   gcloud auth activate-service-account --key-file="${service_key_path}"
   gcloud config set project "${GCP_PROJECT_ID}"
 
-  bbl_name_servers_json="$( bbl lbs --json | jq -r '.cf_system_domain_dns_servers' )"
-  bbl_name_servers_raw="$( echo "${bbl_name_servers_json}" | jq -r 'join(" ")' )"
-  gcp_name_servers_json="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json )"
-  gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
+  if [ "${IS_BOSH_LITE}" == "false" ]; then
+    bbl_name_servers_json="$( bbl lbs --json | jq -r '.cf_system_domain_dns_servers' )"
+    bbl_name_servers_raw="$( echo "${bbl_name_servers_json}" | jq -r 'join(" ")' )"
+    gcp_name_servers_json="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json )"
+    gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
 
-  record_count="$( echo "${gcp_name_servers_json}" | jq 'length' )"
-  if [ "${record_count}" != "0" ]; then
-    gcp_name_servers_raw="$( echo "${gcp_name_servers_json}" | jq -r '.[0].rrdatas | join(" ")' )"
-    gcloud dns record-sets transaction remove --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${gcp_name_servers_raw}
+    record_count="$( echo "${gcp_name_servers_json}" | jq 'length' )"
+    if [ "${record_count}" != "0" ]; then
+      gcp_name_servers_raw="$( echo "${gcp_name_servers_json}" | jq -r '.[0].rrdatas | join(" ")' )"
+      gcloud dns record-sets transaction remove --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${gcp_name_servers_raw}
+    fi
+
+    gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${bbl_name_servers_raw}
+    gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
+  else
+    gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
+
+    gcp_records_json="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "${DNS_DOMAIN}" --format=json )"
+    record_count="$( echo "${gcp_records_json}" | jq 'length' )"
+    if [ "${record_count}" != "0" ]; then
+      existing_record_ip="$( echo "${gcp_records_json}" | jq -r '.[0].rrdatas | join(" ")' )"
+      gcloud dns record-sets transaction remove --name "${DNS_DOMAIN}" --type=A --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 "${existing_record_ip}"
+    fi
+
+    director_external_ip="$(jq -r .tfState ./bbl-state.json | jq -r .modules[0].outputs.external_ip.value)"
+    gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=A --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 "${director_external_ip}"
+    gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
   fi
-
-  gcloud dns record-sets transaction add --name "${DNS_DOMAIN}" --type=NS --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 ${bbl_name_servers_raw}
-  gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}"
 }
 
 pushd "${bbl_state_dir}" > /dev/null
