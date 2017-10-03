c3cca0d84c04117f4d7377c766961120431b1c5f
Tim Downey <tdowney@pivotal.io>
2017-10-03 15:07:54 -0700

    Add ops-file to add credhub-lb vm_extension to Elsa cloud-config
    [#151563840]
    
    Signed-off-by: Annie Sing <asing@pivotal.io>

---
+++ ci/bbl-tasks/update-cloud-config-with-credhub.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+function setup_bosh_env_vars() {
+  echo "Setting env vars..."
+  pushd "bbl-state/${BBL_STATE_DIR}"
+    export BOSH_CA_CERT="$(bbl director-ca-cert)"
+    export BOSH_ENVIRONMENT=$(bbl director-address)
+    export BOSH_CLIENT=$(bbl director-username)
+    export BOSH_CLIENT_SECRET=$(bbl director-password)
+  popd
+}
+
+function update_bosh_cloud_config() {
+  echo "Updating bosh cloud-config on ${BOSH_ENVIRONMENT}..."
+  bosh -n update-cloud-config <(bbl cloud-config) \
+    -o capi-ci/cf-deployment-operations/temporary/credhub-lb-cloud-properties.yml
+}
+
+function main() {
+  setup_bosh_env_vars
+  update_bosh_cloud_config
+  echo "Done"
+}
+
+main
