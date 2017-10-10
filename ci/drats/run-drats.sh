66eb379c8f5fb579cf6205c1bbfbbde0a5480357
Raina Masand <rmasand@pivotal.io>
2017-10-10 15:54:51 -0700

    Change BOSH_URL to BOSH_ENVIRONMENT for run-drats
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- ci/drats/run-drats.sh
+++ ci/drats/run-drats.sh
@@ -21,14 +21,14 @@ mkdir -p "${drats_dir}"
 cp -a "${drats_src}/." "${drats_dir}"
 
 env_file="${environment_dir}/metadata"
-BOSH_URL="$(jq -e -r .target "${env_file}")"
+BOSH_ENVIRONMENT="$(jq -e -r .target "${env_file}")"
 BOSH_CLIENT="$(jq -e -r .client "${env_file}")"
 BOSH_CLIENT_SECRET="$(jq -e -r .client_secret "${env_file}")"
 BOSH_CA_CERT="$(jq -e -r .ca_cert "${env_file}")"
 BOSH_GW_USER="$(jq -e -r .gw_user "${env_file}")"
 BOSH_GW_HOST="$(jq -e -r .gw_host "${env_file}")"
 BOSH_GW_PRIVATE_KEY_CONTENTS="$(jq -e -r .gw_private_key "${env_file}")"
-export BOSH_URL BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
+export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT
 
 CF_ADMIN_PASSWORD="$(bosh interpolate "${vars_store_dir}/${VARS_STORE_PATH}" --path=/cf_admin_password)"
 export CF_ADMIN_PASSWORD
