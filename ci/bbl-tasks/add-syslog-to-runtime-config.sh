8a93662739f91c68be0cf6289773caf1157ee753
Annie Sing <asing@pivotal.io>
2017-10-17 10:29:50 -0700

    Add syslog runtime config to update-mulan
    [#151811590]
    
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

---
+++ ci/bbl-tasks/add-syslog-to-runtime-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+function setup_bosh_env_vars() {
+  echo "Setting env vars..."
+  pushd "bbl-state/${BBL_STATE_DIR}"
+    export BOSH_CA_CERT="$(bbl director-ca-cert)"
+    export BOSH_ENVIRONMENT=$(bbl director-address)
+    export BOSH_CLIENT=$(bbl director-username)
+    export BOSH_CLIENT_SECRET=$(bbl director-password)
+  popd
+}
+
+function update_bosh_runtime_config() {
+  echo "Updating bosh cloud-config on ${BOSH_ENVIRONMENT}..."
+  pushd "bbl-state/${BBL_STATE_DIR}"
+    bosh -n update-runtime-config ../../capi-ci/bosh-deployment-files/use-syslog-release.yml
+  popd
+}
+
+function main() {
+  setup_bosh_env_vars
+  update_bosh_runtime_config
+  echo "Done"
+}
+
+main
