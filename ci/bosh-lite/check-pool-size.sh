9d1f4330d8777592b41ed485b64d89644e70d050
Elena Sharma <esharma@pivotal.io>
2017-10-31 16:05:49 -0700

    Count building pool in check-pool-size
    ...to decide when to trigger create-bosh-lite
    
    [#149099325]
    
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- ci/bosh-lite/check-pool-size.sh
+++ ci/bosh-lite/check-pool-size.sh
@@ -5,6 +5,7 @@ set -eu
 # ENV
 : "${MIN_UNCLAIMED_COUNT:?}"
 : "${POOL_NAME:="bosh-lites"}"
+: "${BUILDING_POOL_NAME:="building-bosh-lites"}"
 : "${GIT_USERNAME:="CAPI CI"}"
 : "${GIT_EMAIL:="cf-capi-eng+ci@pivotal.io"}"
 
@@ -20,6 +21,8 @@ git clone "${pool_dir}" "${output_dir}"
 
 pushd "${output_dir}" > /dev/null
   unclaimed_count="$(find "${POOL_NAME}/unclaimed" -not -path '*/\.*' -type f | wc -l)"
+  unclaimed_count+="$(find "${BUILDING_POOL_NAME}/unclaimed" -not -path '*/\.*' -type f | wc -l)"
+
   if [ "${unclaimed_count}" -lt "${MIN_UNCLAIMED_COUNT}" ]; then
     # The create-bosh-lite job watches this file for changes
     date +%s > .trigger-bosh-lites-create
@@ -27,7 +30,7 @@ pushd "${output_dir}" > /dev/null
     git config user.name "${GIT_USERNAME}"
     git config user.email "${GIT_EMAIL}"
     git add .trigger-bosh-lites-create
-    git commit -m "Not enough unclaimed envs in ${POOL_NAME} pool, updating trigger."
+    git commit -m "Not enough unclaimed envs in ${POOL_NAME} or ${BUILDING_POOL_NAME} pools, updating trigger."
   fi
 popd > /dev/null
 
