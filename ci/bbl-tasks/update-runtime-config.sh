7a60453615123047ab4752dd69a3a5e9e2127c7b
Jeff Pak <jeffrey.pak@dell.com>
2018-05-11 15:31:34 -0700

    more bbl6 changes
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- ci/bbl-tasks/update-runtime-config.sh
+++ ci/bbl-tasks/update-runtime-config.sh
@@ -4,10 +4,15 @@ set -eu
 function setup_bosh_env_vars() {
   echo "Setting env vars..."
   pushd "bbl-state/${BBL_STATE_DIR}"
-    export BOSH_CA_CERT="$(bbl director-ca-cert)"
-    export BOSH_ENVIRONMENT=$(bbl director-address)
-    export BOSH_CLIENT=$(bbl director-username)
-    export BOSH_CLIENT_SECRET=$(bbl director-password)
+    bblver="$(bbl -v | cut -d' ' -f2 | cut -d'.' -f1)"
+    if [ $bblver -eq "6" ]; then
+      eval "$(bbl print-env)"
+    else
+      export BOSH_CA_CERT="$(bbl director-ca-cert)"
+      export BOSH_ENVIRONMENT=$(bbl director-address)
+      export BOSH_CLIENT=$(bbl director-username)
+      export BOSH_CLIENT_SECRET=$(bbl director-password)
+    fi
   popd
 }
 
