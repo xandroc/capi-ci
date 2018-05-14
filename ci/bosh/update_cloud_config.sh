c52a1cc1fc37f5075f9625b4c1fb49cb55369ebb
Jeff Pak <jeffrey.pak@dell.com>
2018-05-14 11:24:32 -0700

    Readd script_dir var to update_cloud_config.sh
    [#155957637]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- ci/bosh/update_cloud_config.sh
+++ ci/bosh/update_cloud_config.sh
@@ -10,6 +10,7 @@ pushd "bbl-state/${BBL_STATE_DIR}"
   eval "$(bbl print-env)"
 popd
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../.." && pwd )"
 cloud_config="${workspace_dir}/cloud-config/${CLOUD_CONFIG_PATH}"
 
