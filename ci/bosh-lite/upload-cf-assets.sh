3762b6e3ca8622117a805e3f494e479f114fa1f2
Isobel Redelmeier <iredelmeier@pivotal.io>
2018-08-07 15:08:28 -0700

    Upload both trusty and xenial to bosh lites
    [#159605144]
    
    Signed-off-by: Nick Wei <nwei@pivotal.io>

--- ci/bosh-lite/upload-cf-assets.sh
+++ ci/bosh-lite/upload-cf-assets.sh
@@ -11,9 +11,13 @@ env_info="${workspace_dir}/bosh-lite-env-info"
 source "${env_info}/metadata"
 
 echo "Uploading stemcell..."
-stemcell_version=$(bosh interpolate --path /stemcells/alias=default/version "${cf_deployment_repo}/cf-deployment.yml")
+trusty_stemcell_version=$(bosh interpolate --path /stemcells/alias=default/version "${cf_deployment_repo}/cf-deployment.yml")
 bosh -n upload-stemcell \
-  "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=${stemcell_version}"
+  "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=${trusty_stemcell_version}"
+
+xenial_stemcell_version=$(bosh interpolate --path /stemcells/alias=default/version "${cf_deployment_repo}/operations/experimental/use-xenial-stemcell.yml")
+bosh -n upload-stemcell \
+  "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent?v=${xenial_stemcell_version}"
 
 echo "Uploading cloud-config..."
 bosh -n update-cloud-config "${cf_deployment_repo}/iaas-support/bosh-lite/cloud-config.yml"
