110c51718dda9fb70c735c2295dff7141f80aa72
Davis W. Frank <dwfrank@pivotal.io>
2018-09-04 14:28:59 -0700

    CD like an animal; so that we get the correct exit code
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- ci/snyk-test/run-snyk-test.sh
+++ ci/snyk-test/run-snyk-test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-pushd cloud_controller_ng
-  npm i -g snyk  #TODO: not this
-  snyk auth $SNYK_TOKEN
-  snyk test
-popd
-
+cd cloud_controller_ng
+npm i -g snyk  #TODO: not this
+snyk auth $SNYK_TOKEN
+snyk test
