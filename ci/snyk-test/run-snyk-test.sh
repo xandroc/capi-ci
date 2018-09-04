c0caf1ca7b4920417f37e7c5e0233b226618c0fb
Charles Hansen <chansen@pivotal.io>
2018-09-04 14:26:40 -0700

    run snyk in cc directory [#160010140]
    Signed-off-by: Davis W. Frank <dwfrank@pivotal.io>

--- ci/snyk-test/run-snyk-test.sh
+++ ci/snyk-test/run-snyk-test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-npm i -g snyk  #TODO: not this
-snyk auth $SNYK_TOKEN
-snyk test
-
+pushd cloud_controller_ng
+  npm i -g snyk  #TODO: not this
+  snyk auth $SNYK_TOKEN
+  snyk test
+popd
 
