83025419fba84e9ff2409c1ffd6fc58b781a141e
Eli Wrenn <ewrenn@pivotal.io>
2018-09-07 10:55:47 -0700

    make xena-mysql-rotate-ccdb tasks use credhub
    Signed-off-by: Connor Braa <cbraa@pivotal.io>

--- ci/cf/verify-env-vars.sh
+++ ci/cf/verify-env-vars.sh
@@ -3,22 +3,27 @@ set -eu
 
 # ENV
 : "${CF_API_URL:?}"
+: "${CF_DEPLOYMENT_NAME:=cf}"
 : "${APP_NAME:=?}"
 : "${APP_ENV_VAR_NAME:=?}"
 : "${APP_ENV_VAR_VALUE:=?}"
 
-VARS_STORE_PATH=vars-store.yml
 CF_ADMIN_USERNAME=admin
 ORG_NAME=test-key-rotation-org
 SPACE_NAME=test-key-rotation-space
 
+pushd "capi-ci-private/${BBL_STATE_DIR}"
+  eval "$(bbl print-env)"
+  export DIRECTOR_NAME="$(jq -e -r .bosh.directorName bbl-state.json)"
+  unset BOSH_ALL_PROXY
+popd
+
 # INPUTS
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
-vars_store_dir="${workspace_dir}/vars-store"
 staticfile_app_dir="${workspace_dir}/staticfile-app"
 
-CF_ADMIN_PASSWORD="$(bosh interpolate "${vars_store_dir}/${VARS_STORE_PATH}" --path=/cf_admin_password)"
+CF_ADMIN_PASSWORD="$(credhub get --path=/${DIRECTOR_NAME}/${CF_DEPLOYMENT_NAME}/cf_admin_password)"
 
 echo "Logging in and setting up..."
 cf api $CF_API_URL --skip-ssl-validation
