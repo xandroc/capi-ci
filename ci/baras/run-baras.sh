a8f4b7fbdbc3d82b0db7516e509f52bd59c3bb44
Chris Selzo <cselzo@pivotal.io>
2018-10-12 14:33:44 -0700

    Add BARAs to mulan
    [#160361713]
    
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

---
+++ ci/baras/run-baras.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -xeu
+
+build_dir=${PWD}
+
+export CONFIG
+CONFIG=$(mktemp)
+
+original_config="${build_dir}/integration-config/${CONFIG_FILE_PATH}"
+cp ${original_config} ${CONFIG}
+
+CF_GOPATH=/go/src/github.com/cloudfoundry/
+
+echo "Moving capi-bara-tests onto the gopath..."
+mkdir -p $CF_GOPATH
+cp -R capi-bara-tests $CF_GOPATH
+
+cd /go/src/github.com/cloudfoundry/capi-bara-tests
+
+export CF_DIAL_TIMEOUT=11
+
+export CF_PLUGIN_HOME=$HOME
+
+./bin/test \
+-keepGoing \
+-randomizeAllSpecs \
+-skipPackage=helpers \
+-slowSpecThreshold=120 \
+-nodes="${NODES}" \
+-noisySkippings=false
