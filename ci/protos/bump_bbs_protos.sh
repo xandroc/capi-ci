7fb780e6aea1bde937be71cdd46a27ec45d151bf
Charles Hansen <chansen@pivotal.io>
2018-10-24 12:53:51 -0700

    refactor bump_bbs_protos.sh

--- ci/protos/bump_bbs_protos.sh
+++ ci/protos/bump_bbs_protos.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
+ruby_generated_files_path="${PWD}/cloud_controller_ng/lib/diego/bbs/models"
+
 pushd capi-release
   source .envrc
-  ./src/cloud_controller_ng/scripts/generate-bbs-models.sh
+
+  bbs_models_path="src/code.cloudfoundry.org/bbs/models"
+
+  pushd "${GOPATH}/${bbs_models_path}"
+    # the ruby modules are created based on the package name
+    sed -i'' -e 's/package models/package diego.bbs.models/' ./*.proto
+
+    protoc --proto_path="${GOPATH}/src":. --ruby2_out="${ruby_generated_files_path}" ./*.proto
+    git checkout .
+  popd
 popd
 
-cp -r capi-release/src/cloud_controller_ng bumped/cloud_controller_ng
+pushd cloud_controller_ng
+  git add ${ruby_generated_files_path}
+  git config user.name 'CAPI CI'
+  git config user.email cf-capi-eng+ci@pivotal.io
+  git commit -m "Bump bbs protos"
+popd
+
+cp -r cloud_controller_ng bumped/cloud_controller_ng
