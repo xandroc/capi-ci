806ef175c5c8468c123848b6cf45fb4c8caa89bf
Charles Hansen <chansen@pivotal.io>
2018-10-25 09:00:38 -0700

    bump_bbs_protos uses the ruby protobuf version from cc_ng [#161227172]

--- ci/protos/bump_bbs_protos.sh
+++ ci/protos/bump_bbs_protos.sh
@@ -2,6 +2,14 @@
 
 ruby_generated_files_path="${PWD}/cloud_controller_ng/lib/diego/bbs/models"
 
+pushd cloud_controller_ng
+  # protoc supports ruby natively, but only proto3,
+  # We need to create a "new" language (called ruby2) that uses the ruby protobuff gem
+  # instead of the native protoc ruby compiler
+  # see bbs https://github.com/cloudfoundry/bbs#generating-ruby-models-for-bbs-models
+  cp $(bundle exec which protoc-gen-ruby) /usr/local/bin/protoc-gen-ruby2
+popd
+
 pushd capi-release
   source .envrc
 
