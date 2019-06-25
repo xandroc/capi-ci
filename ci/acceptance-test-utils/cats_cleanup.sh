397a8d351a9e129dc442e671dba66362c2978a31
Tim Downey <tdowney@pivotal.io>
2019-06-25 16:09:48 -0700

    Run cats_cleanup after mulan environment tests
    [#166889284](https://www.pivotaltracker.com/story/show/166889284)
    
    Co-authored-by: Tim Downey <tdowney@pivotal.io>
    Co-authored-by: Mona Mohebbi <mmohebbi@pivotal.io>

---
+++ ci/acceptance-test-utils/cats_cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eu
+
+# ENV
+: "${CF_API_TARGET:?}"
+
+CF_ADMIN_USERNAME=admin
+
+pushd "capi-ci-private/${BBL_STATE_DIR}"
+  eval "$(bbl print-env)"
+  export DIRECTOR_NAME="$(jq -e -r .bosh.directorName bbl-state.json)"
+  unset BOSH_ALL_PROXY
+popd
+
+# INPUTS
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
+
+CF_ADMIN_PASSWORD="$(credhub get --name=/${DIRECTOR_NAME}/${CF_DEPLOYMENT_NAME}/cf_admin_password -j | jq .value -r)"
+
+echo "Logging in and setting up..."
+cf api $CF_API_TARGET --skip-ssl-validation
+cf auth admin "$CF_ADMIN_PASSWORD"
+
+echo "Running cats cleanup script"
+pushd "capi-workspace/scripts"
+  ./cats_cleanup
+popd
+
+echo "Success!"
