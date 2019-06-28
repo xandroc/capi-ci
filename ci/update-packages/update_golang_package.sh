87d0dc377447626d1188d723e24aefbb594df04b
Renee Chu <rchu@pivotal.io>
2019-06-28 13:57:52 -0700

    Add job to auto-bump vendored golang package
    [#163641118]
    
    Co-authored-by: Renee Chu <rchu@pivotal.io>
    Co-authored-by: Chris Selzo <cselzo@pivotal.io>

---
+++ ci/update-packages/update_golang_package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -x
+
+cp $PWD/capi-ci-private/ci/private.yml $PWD/capi-release/config/private.yml
+
+pushd golang-release
+	new_go_version=$(bosh blobs | grep linux | grep go1. | cut -d . -f 1-3 | sort | tail -1)
+popd
+
+cd capi-release
+
+bosh vendor-package golang-1.12-linux ../golang-release
+
+git --no-pager diff packages .final_builds
+
+git config user.name "CAPI CI"
+git config user.email "cf-capi-eng+ci@pivotal.io"
+
+git add -A packages .final_builds
+git commit -n -m "Bump Golang to $new_go_version"
+rsync -av $PWD/ ../updated-capi-release
