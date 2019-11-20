0f5e0825240f7997e6a444e0b032c97def08dc20
Mona Mohebbi <mmohebbi@pivotal.io>
2019-11-20 13:36:47 -0800

    Undo cert-related changes to bosh-lite terraform file
    We tried last week to set up our bosh-lites with certificates provided
    by Lets Encrypt, but this led to lots of issues for people who were
    sharing this repo. We may revisit this at a later date now that we know
    more about the problems it can cause.
    
    [#169793167]
    
    Co-authored-by: Mona Mohebbi <mmohebbi@pivotal.io>
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- terraform/bosh-lite/bosh-lite.tf
+++ terraform/bosh-lite/bosh-lite.tf
@@ -20,37 +20,33 @@ variable "system_domain_suffix" {}
 
 # RESOURCES
 
-provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
-
 provider "google" {
-  credentials = var.json_key
-  project = var.project_id
-  region = var.region
+  credentials = "${var.json_key}"
+  project = "${var.project_id}"
+  region = "${var.region}"
 }
 
 provider "google" {
   alias = "dns"
-  credentials = var.dns_json_key
-  project = var.dns_project_id
-  region = var.region
+  credentials = "${var.dns_json_key}"
+  project = "${var.dns_project_id}"
+  region = "${var.region}"
 }
 
 resource "google_compute_network" "default" {
-  name = var.env_name
+  name = "${var.env_name}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "default" {
-  name = var.env_name
-  ip_cidr_range = var.internal_cidr
-  network = google_compute_network.default.self_link
+  name = "${var.env_name}"
+  ip_cidr_range = "${var.internal_cidr}"
+  network = "${google_compute_network.default.self_link}"
 }
 
 resource "google_compute_firewall" "default" {
-  name = var.env_name
-  network = google_compute_network.default.name
+  name = "${var.env_name}"
+  network = "${google_compute_network.default.name}"
 
   allow {
     protocol = "icmp"
@@ -67,40 +63,17 @@ resource "google_compute_firewall" "default" {
 }
 
 resource "google_compute_address" "default" {
-  name = var.env_name
+  name = "${var.env_name}"
 }
 
 resource "google_dns_record_set" "default" {
-  provider = google.dns
+  provider = "google.dns"
   name = "*.${var.env_name}.${var.system_domain_suffix}."
   type = "A"
   ttl = 300
 
-  managed_zone = var.dns_zone_name
+  managed_zone = "${var.dns_zone_name}"
   rrdatas = [ "${google_compute_address.default.address}" ]
-}
-
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
-
-resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = "cf-v3-acceleration@pivotal.io"
-}
-
-resource "acme_certificate" "certificate" {
-  account_key_pem           = acme_registration.reg.account_key_pem
-  common_name = "*.${var.env_name}.${var.system_domain_suffix}"
-
-  dns_challenge {
-    provider = "gcloud"
-    config = {
-      GCE_PROJECT = "cf-cli"
-      GCE_SERVICE_ACCOUNT = "${var.json_key}"
-      GCE_PROPAGATION_TIMEOUT = "600"
-    }
-  }
 }
 
 # OUTPUTS
@@ -143,12 +116,4 @@ output "internal_gw" {
 
 output "internal_cidr" {
   value = "${google_compute_subnetwork.default.ip_cidr_range}"
-}
-
-output "CF_TLS_key" {
-  value = "${acme_certificate.certificate.private_key_pem}"
-}
-
-output "CF_TLS_cert" {
-  value = "${acme_certificate.certificate.certificate_pem}"
 }
