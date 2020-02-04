a7cedc878f2df6aa58f1b5d19a6030e8e70b809d
Chris Tarazi <ctarazi@pivotal.io>
2020-02-04 15:55:02 -0800

    Add auth to apply-kpack task
    [#171063670]
    
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Sannidhi Jalukar <sjalukar@pivotal.io>

--- ci/k8s/apply-kpack.sh
+++ ci/k8s/apply-kpack.sh
@@ -2,6 +2,14 @@
 
 set -eux -o pipefail
 
-source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
+touch kubeconfig
+export KUBECONFIG="$(pwd)/kubeconfig"
 
-KUBECONFIG="capi-ci-private/${CAPI_ENVIRONMENT_NAME}/kubeconfig" kubectl apply -f "${KUBECTL_YAML_FILE}"
+gcloud auth activate-service-account \
+  "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
+  --key-file="capi-ci-private/${CAPI_ENVIRONMENT_NAME}/concourse-service-account.json" \
+  --project="${GOOGLE_PROJECT_NAME}"
+
+gcloud --project=cf-capi-arya container clusters get-credentials --zone us-central1-a samus-cluster
+
+kubectl apply -f "${KUBECTL_YAML_FILE}"
