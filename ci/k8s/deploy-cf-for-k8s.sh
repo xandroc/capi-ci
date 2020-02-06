ea6b6b49fb5bbcc0a0dc9011ac6f319cc4a5d670
Chris Tarazi <ctarazi@pivotal.io>
2020-02-06 11:43:20 -0800

    Add key-file as a param to the task
    This reduces coupling with the capi-ci-private directory layout.
    
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    
    [#171063670]

--- ci/k8s/deploy-cf-for-k8s.sh
+++ ci/k8s/deploy-cf-for-k8s.sh
@@ -4,7 +4,7 @@ set -eux -o pipefail
 
 gcloud auth activate-service-account \
   "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
-  --key-file="capi-ci-private/${CAPI_ENVIRONMENT_NAME}/concourse-service-account.json" \
+  --key-file="${GOOGLE_KEY_FILE_PATH}" \
   --project="${GOOGLE_PROJECT_NAME}"
 
 source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
