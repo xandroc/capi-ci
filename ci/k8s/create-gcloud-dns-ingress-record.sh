fed620a8dee8c9b9976c3a7cb2ad1162167663b7
Chris Tarazi <ctarazi@pivotal.io>
2020-02-06 14:55:25 -0800

    Make dns record manipulation idempotent
    [#171063670]
    
    Co-authored-by: Chris Tarazi <ctarazi@pivotal.io>
    Co-authored-by: Connor Braa <cbraa@pivotal.io>

--- ci/k8s/create-gcloud-dns-ingress-record.sh
+++ ci/k8s/create-gcloud-dns-ingress-record.sh
@@ -9,13 +9,19 @@ gcloud auth activate-service-account \
 
 source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
 
-# TODO: kubectl get that ingress
+NAME="*.${CF_DOMAIN}."
 INGRESS_IP="$(kubectl get svc -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[*].ip}')"
+
+set +e
+PREVIOUS_RECORD_IP="$(gcloud dns record-sets list --zone arya-zone | grep ${CF_DOMAIN} | awk '{ print $4 }')"
+set -e
 
 gcloud dns record-sets transaction start --zone="${GOOGLE_DNS_ZONE}"
 
-gcloud dns record-sets transaction add "${INGRESS_IP}" --name=\*."${CF_DOMAIN}". --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
+if [[ -n "${PREVIOUS_RECORD_IP}" ]]; then
+  gcloud dns record-sets transaction remove "${PREVIOUS_RECORD_IP}" --name "${NAME}" --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
+fi
 
-gcloud dns record-sets transaction remove "${INGRESS_IP}" --name=\*."${CF_DOMAIN}". --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
+gcloud dns record-sets transaction add "${INGRESS_IP}" --name "${NAME}" --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
 
 gcloud dns record-sets transaction execute --zone="${GOOGLE_DNS_ZONE}"
