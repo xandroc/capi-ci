f55317825a06d175effcbd870593ddec9eb79903
Chris Tarazi <ctarazi@pivotal.io>
2020-02-07 16:34:25 -0800

    Add task for running smoke tests
    [#171063670]
    
    Authored-by: Chris Tarazi <ctarazi@pivotal.io>

---
+++ ci/k8s/run-smoke-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+gcloud auth activate-service-account \
+  "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
+  --key-file="${GOOGLE_KEY_FILE_PATH}" \
+  --project="${GOOGLE_PROJECT_NAME}"
+
+source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
+
+pushd "cf-for-k8s"
+  ./bin/run-smoke-tests.sh
+popd
