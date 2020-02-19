3c69e0500257f4064b19bb44f331d5a52c8213bc
Seth Boyles <sboyles@pivotal.io>
2020-02-19 10:56:33 -0800

    Try deleting deployments from samus cluster before redeploying
    [#171264248]
    
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

---
+++ ci/k8s/delete-cf-for-k8s.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+#gcloud auth activate-service-account \
+#  "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
+#  --key-file="${GOOGLE_KEY_FILE_PATH}" \
+#  --project="${GOOGLE_PROJECT_NAME}"
+
+source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
+pushd "cf-for-k8s"
+  kapp delete -a cf --yes
+popd
