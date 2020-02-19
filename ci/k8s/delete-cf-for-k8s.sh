eed8143dc062faece662b1c1ed927ef802a576dd
Seth Boyles <sboyles@pivotal.io>
2020-02-19 11:02:17 -0800

    Add gcloud crendentials to kapp delete step
    [#171264248]
    
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- ci/k8s/delete-cf-for-k8s.sh
+++ ci/k8s/delete-cf-for-k8s.sh
@@ -2,10 +2,10 @@
 
 set -eux -o pipefail
 
-#gcloud auth activate-service-account \
-#  "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
-#  --key-file="${GOOGLE_KEY_FILE_PATH}" \
-#  --project="${GOOGLE_PROJECT_NAME}"
+gcloud auth activate-service-account \
+  "${GOOGLE_SERVICE_ACCOUNT_EMAIL}" \
+  --key-file="${GOOGLE_KEY_FILE_PATH}" \
+  --project="${GOOGLE_PROJECT_NAME}"
 
 source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
 pushd "cf-for-k8s"
