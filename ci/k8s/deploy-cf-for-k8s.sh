e38aca84cb34e9a7ae6dff30168f139b013468a9
Connor Braa <cbraa@pivotal.io>
2020-02-20 10:56:10 -0800

    generate bara config in deploy-cf-for-k8s
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- ci/k8s/deploy-cf-for-k8s.sh
+++ ci/k8s/deploy-cf-for-k8s.sh
@@ -12,6 +12,16 @@ pushd "cf-for-k8s"
   hack/generate-values.sh "${CAPI_ENVIRONMENT_NAME}.capi.land" > cf-install-values.yml
   bin/install-cf.sh ./cf-install-values.yml
 popd
+
 bosh interpolate --path /cf_admin_password cf-for-k8s/cf-install-values.yml > env-metadata/cf-admin-password.txt
 echo "${CAPI_ENVIRONMENT_NAME}.capi.land" > env-metadata/dns-domain.txt
 
+cat > env-metadata/integration-config.json << EOF
+{
+  "api": "api.${CAPI_ENVIRONMENT_NAME}.capi.land",
+  "apps_domain": "${CAPI_ENVIRONMENT_NAME}.capi.land",
+  "admin_user": "admin",
+  "admin_password": "$(cat env-metadata/cf-admin-password.txt)",
+  "skip_ssl_validation": true
+}
+EOF
