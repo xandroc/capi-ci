63d21a11a7457e5584a1715a4dbbab83e10f9052
Piyali Banerjee <pbanerjee@pivotal.io>
2020-02-20 14:48:23 -0800

    sleep for the ttl after setting dns
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>
    Co-authored-by: Connor Braa <cbraa@pivotal.io>

--- ci/k8s/create-gcloud-dns-ingress-record.sh
+++ ci/k8s/create-gcloud-dns-ingress-record.sh
@@ -11,6 +11,7 @@ source "capi-ci-private/${CAPI_ENVIRONMENT_NAME}/.envrc"
 
 NAME="*.${CF_DOMAIN}."
 INGRESS_IP="$(kubectl get svc -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[*].ip}')"
+DNS_TTL=15
 
 set +e
 PREVIOUS_RECORD_IP="$(gcloud dns record-sets list --zone ${GOOGLE_DNS_ZONE} | grep ${NAME} | awk '{ print $4 }')"
@@ -19,9 +20,11 @@ set -e
 gcloud dns record-sets transaction start --zone="${GOOGLE_DNS_ZONE}"
 
 if [[ -n "${PREVIOUS_RECORD_IP}" ]]; then
-  gcloud dns record-sets transaction remove "${PREVIOUS_RECORD_IP}" --name "${NAME}" --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
+  gcloud dns record-sets transaction remove "${PREVIOUS_RECORD_IP}" --name "${NAME}" --ttl="${DNS_TTL}" --type=A --zone="${GOOGLE_DNS_ZONE}"
 fi
 
-gcloud dns record-sets transaction add "${INGRESS_IP}" --name "${NAME}" --ttl=300 --type=A --zone="${GOOGLE_DNS_ZONE}"
+gcloud dns record-sets transaction add "${INGRESS_IP}" --name "${NAME}" --ttl="${DNS_TTL}" --type=A --zone="${GOOGLE_DNS_ZONE}"
 
 gcloud dns record-sets transaction execute --zone="${GOOGLE_DNS_ZONE}"
+
+sleep "${DNS_TTL}"
