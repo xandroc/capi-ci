10c1d20f3e5b1354c5642a9a2db232de3caeedf2
Connor Braa <cbraa@pivotal.io>
2020-08-31 15:09:48 -0700

    go get AND go install ginkgo
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- ci/k8s/run-package-image-uploader-unit-tests.sh
+++ ci/k8s/run-package-image-uploader-unit-tests.sh
@@ -6,7 +6,9 @@ set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 
+go get github.com/onsi/ginkgo/ginkgo
 go install github.com/onsi/ginkgo/ginkgo
+
 pushd "${workspace_dir}/package-image-uploader/src/package-image-uploader" >/dev/null
      ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
 popd >/dev/null
