5654ba0c6415e8800d8a7ebbddac245a39d2420c
Connor Braa <cbraa@pivotal.io>
2020-08-31 15:06:38 -0700

    install ginkgo when running package-image-uploader tests
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- ci/k8s/run-package-image-uploader-unit-tests.sh
+++ ci/k8s/run-package-image-uploader-unit-tests.sh
@@ -6,6 +6,7 @@ set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 
+go install github.com/onsi/ginkgo/ginkgo
 pushd "${workspace_dir}/package-image-uploader/src/package-image-uploader" >/dev/null
-    ginkgo -r -p
+     ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
 popd >/dev/null
