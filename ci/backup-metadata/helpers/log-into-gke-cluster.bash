dd51008d7f06341cf448ec12dca05520949f2bc8
Neil Hickey <nhickey@pivotal.io>
2020-09-01 10:54:52 +0000

    Add --project flag to log-into-gke-cluster
    + Without this, the ci was failing on the wrong project
    
    [#174212563]

--- ci/backup-metadata/helpers/log-into-gke-cluster.bash
+++ ci/backup-metadata/helpers/log-into-gke-cluster.bash
@@ -10,4 +10,4 @@ cluster_name=$(cat $ROOT_DIR/cluster-name/info)
 project_name=$(jq -r .project_id $ROOT_DIR/service-account.json)
 
 gcloud auth activate-service-account --key-file=$ROOT_DIR/service-account.json
-gcloud container clusters get-credentials "${cluster_name}" --zone europe-west1-b
+gcloud container clusters get-credentials "${cluster_name}" --zone europe-west1-b --project "${project_name}"
