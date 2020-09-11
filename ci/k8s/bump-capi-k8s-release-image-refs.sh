5b4a505913f84f72a54c4dd49b281bf2ec2da012
Connor Braa <cbraa@pivotal.io>
2020-09-11 13:40:13 -0700

    use new shortlog script in CI
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- ci/k8s/bump-capi-k8s-release-image-refs.sh
+++ ci/k8s/bump-capi-k8s-release-image-refs.sh
@@ -10,20 +10,15 @@ function get_image_digest_for_resource () {
   popd >/dev/null
 }
 
-function git_sha () {
-  pushd $1 >/dev/null
-    git rev-parse HEAD
-  popd >/dev/null
-}
-
 CAPI_IMAGE="cloudfoundry/cloud-controller-ng@$(get_image_digest_for_resource capi-docker-image)"
 NGINX_IMAGE="cloudfoundry/capi-nginx@$(get_image_digest_for_resource nginx-docker-image)"
 CONTROLLERS_IMAGE="cloudfoundry/cf-api-controllers@$(get_image_digest_for_resource cf-api-controllers-docker-image)"
 PACKAGE_IMAGE_UPLOADER_IMAGE="cloudfoundry/cf-api-package-image-uploader@$(get_image_digest_for_resource package-image-uploader-docker-image)"
-CAPI_SHA="$(git_sha cloud_controller_ng)"
-NGINX_SHA="$(git_sha capi-nginx)"
-CONTROLLERS_SHA="$(git_sha cf-api-controllers)"
-PACKAGE_IMAGE_UPLOADER_SHA="$(git_sha package-image-uploader)"
+
+CCNG_DIR="cloud_controller_ng"
+CF_API_CONTROLLERS_DIR="cf-api-controllers"
+PACKAGE_IMAGE_UPLOADER_DIR="package-image-uploader"
+NGINX_DIR="capi-nginx"
 
 function bump_image_references() {
     cat <<- EOF > "${PWD}/update-images.yml"
@@ -53,38 +48,6 @@ EOF
 
 function make_git_commit() {
     shopt -s dotglob
-
-    cat > "${MESSAGE_FILE}" <<- EOF
-images.yml updated by CI
----
-Updating ccng image to:
-${CAPI_IMAGE}
-
-Built from cloud_controller_ng SHA:
-${CAPI_SHA}
-
----
-Updating nginx image to digest:
-${NGINX_IMAGE}
-
-Built from capi-k8s-release SHA:
-${NGINX_SHA}
-
----
-Updating cf-api-controllers image to digest:
-${CONTROLLERS_IMAGE}
-
-Built from capi-k8s-release SHA:
-${CONTROLLERS_SHA}
-
----
-Updating package-image-uploader image to digest:
-${PACKAGE_IMAGE_UPLOADER_IMAGE}
-
-Built from capi-k8s-release SHA:
-${PACKAGE_IMAGE_UPLOADER_SHA}
-EOF
-
     pushd "capi-k8s-release"
       git config user.name "${GIT_COMMIT_USERNAME}"
       git config user.email "${GIT_COMMIT_EMAIL}"
@@ -92,9 +55,9 @@ EOF
 
       # dont make a commit if there aren't new images
       if ! git diff --cached --exit-code; then
-        cat "${MESSAGE_FILE}"
+        ./scripts/generate-shortlog.sh
         echo "committing!"
-        git commit -F "${MESSAGE_FILE}"
+        git commit -F <(./scripts/generate-shortlog.sh)
       fi
     popd
 
