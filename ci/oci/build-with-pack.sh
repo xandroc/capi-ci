7bb919efcb5e0610364a6e9a36abcf9ec63ba054
Connor Braa <cbraa@pivotal.io>
2020-09-17 20:08:27 -0700

    use newer builder to build ccng image
    [#174523162]
    
    Co-authored-by: Connor Braa <cbraa@pivotal.io>
    Co-authored-by: Piyali Banerjee <pbanerjee@pivotal.io>

--- ci/oci/build-with-pack.sh
+++ ci/oci/build-with-pack.sh
@@ -7,7 +7,7 @@ source capi-ci/ci/docker/common.sh
 start_docker
 
 # parameterizing this is hard in place - ADDITIONAL_ARGS is a hack
-pack build built-image --builder cloudfoundry/cnb:bionic --path "source-repository/${CONTEXT_PATH}" ${ADDITIONAL_ARGS}
+pack build built-image --builder paketobuildpacks/builder:full --path "source-repository/${CONTEXT_PATH}" ${ADDITIONAL_ARGS}
 
 docker save built-image -o image/image.tar
 
