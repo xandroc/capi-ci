a5e65a1a724f0391d4f7955c0599ea1a11300050
Neil Hickey <nhickey@pivotal.io>
2020-10-26 10:39:21 -0700

    begin moving backup-metadata jobs into capi-k8s-release group
    [#175080281]
    
    Co-authored-by: Neil Hickey <nhickey@pivotal.io>
    Co-authored-by: Tim Downey <tdowney@vmware.com>

--- ci/backup-metadata/run-backup-metadata-unit-tests.sh
+++ ci/backup-metadata/run-backup-metadata-unit-tests.sh
@@ -5,6 +5,6 @@ set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 
-pushd "${workspace_dir}/src/src/backup-metadata" >/dev/null
+pushd "${workspace_dir}/backup-metadata/src/backup-metadata" >/dev/null
     make test-unit
 popd >/dev/null
