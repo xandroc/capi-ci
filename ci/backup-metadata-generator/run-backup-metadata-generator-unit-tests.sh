5ea7abc3e0c99eda0b702ee3db1e96de39a80c3f
Tim Downey <tdowney@vmware.com>
2020-11-09 16:42:43 -0800

    rename backup-metadata to backup-metadata-generator
    renamed to make it more clear that this component provides metadata for
    a backup
    
    [#175080281](https://www.pivotaltracker.com/story/show/175080281)
    
    Authored-by: Tim Downey <tdowney@vmware.com>

--- ci/backup-metadata-generator/run-backup-metadata-generator-unit-tests.sh
+++ ci/backup-metadata-generator/run-backup-metadata-generator-unit-tests.sh
@@ -5,6 +5,6 @@ set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 
-pushd "${workspace_dir}/backup-metadata/src/backup-metadata" >/dev/null
+pushd "${workspace_dir}/backup-metadata-generator/src/backup-metadata-generator" >/dev/null
     make test-unit
 popd >/dev/null
--- ci/backup-metadata/e2e-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-./capi-ci/ci/backup-metadata/helpers/log-into-gke-cluster.bash
-
-pushd ./capi-k8s-release/src/backup-metadata
-    make test-e2e
-popd
