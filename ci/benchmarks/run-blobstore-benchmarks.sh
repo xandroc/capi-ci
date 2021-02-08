9111dd96f5d8bf044e14e2f00a4df6df39ee92f9
Belinda Liu <bliu@pivotal.io>
2021-02-08 19:38:11 +0000

    WIP: removing extract-bbl-environment task
    - currently running DRATS will fail when trying to reach the BOSH
    director. It seems that some gateway/jumpbox information is missing from
    one of the environment variables that were removed
    
    [#176829179](https://www.pivotaltracker.com/story/show/176829179)
    
    Authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/benchmarks/run-blobstore-benchmarks.sh
+++ ci/benchmarks/run-blobstore-benchmarks.sh
@@ -12,21 +12,10 @@ workspace_dir="$( cd "${script_dir}/../../.." && pwd )"
 cloud_controller_dir="${workspace_dir}/cloud_controller_ng"
 bbl_vars_file="${workspace_dir}/environment/metadata"
 
-BOSH_ENVIRONMENT="$(jq -e -r .target "${bbl_vars_file}")"
-BOSH_CLIENT="$(jq -e -r .client "${bbl_vars_file}")"
-BOSH_CLIENT_SECRET="$(jq -e -r .client_secret "${bbl_vars_file}")"
-BOSH_CA_CERT="$(jq -e -r .ca_cert "${bbl_vars_file}")"
-BOSH_GW_USER="$(jq -e -r .gw_user "${bbl_vars_file}")"
-BOSH_GW_HOST="$(jq -e -r .gw_host "${bbl_vars_file}")"
-BOSH_GW_PRIVATE_KEY_CONTENTS="$(jq -e -r .gw_private_key "${bbl_vars_file}")"
+JUMPBOX_URL="$(jq -e -r .jumpbox_url "${bbl_vars_file}")" # jumpbox address, includes port which bbl jumpbox-address doesn't
+JUMPBOX_SSH_KEY="$(jq -e -r .jumpbox_ssh_key "${bbl_vars_file}")" # bbl ssh-key
 
-JUMPBOX_URL="$(jq -e -r .jumpbox_url "${bbl_vars_file}")"
-JUMPBOX_SSH_KEY="$(jq -e -r .jumpbox_ssh_key "${bbl_vars_file}")"
-JUMPBOX_USERNAME="$(jq -e -r .jumpbox_username "${bbl_vars_file}")"
-
-export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT \
-  BOSH_GW_USER BOSH_GW_HOST BOSH_GW_PRIVATE_KEY_CONTENTS \
-  JUMPBOX_URL JUMPBOX_SSH_KEY JUMPBOX_USERNAME
+export JUMPBOX_URL JUMPBOX_SSH_KEY
 
 green="$(tput -T xterm-256color setaf 2)"
 reset="$(tput -T xterm-256color sgr0)"
@@ -44,7 +33,7 @@ write_ssh_key() {
   echo "${green}Writing BOSH GW SSH key...${reset}"
 
   key_path="${tmp_dir}/bosh.pem"
-  echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${key_path}"
+  echo "${JUMPBOX_SSH_KEY}" > "${key_path}"
   chmod 600 "${key_path}"
   export BOSH_GW_PRIVATE_KEY="${key_path}"
 }
@@ -52,7 +41,7 @@ write_ssh_key() {
 start_background_ssh_tunnel() {
   echo "${green}Starting background SSH tunnel as SOCKS Proxy...${reset}"
   ssh_jumpbox_url=$(echo "${JUMPBOX_URL}" | cut -d':' -f1)
-  ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -D ${tunnel_port} -fNC ${JUMPBOX_USERNAME}@${ssh_jumpbox_url} -i ${JUMPBOX_PRIVATE_KEY}
+  ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -D ${tunnel_port} -fNC jumpbox@${ssh_jumpbox_url} -i ${JUMPBOX_PRIVATE_KEY}
 }
 
 kill_background_ssh_tunnel() {
--- ci/drats/run-drats.sh
+++ ci/drats/run-drats.sh
@@ -12,50 +12,36 @@ set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workspace_dir="$( cd "${script_dir}/../../../" && pwd )"
 drats_src="${workspace_dir}/drats"
-environment_dir="${workspace_dir}/environment"
+
+tmpdir="$( mktemp -d /tmp/run-drats.XXXXXXXXXX )"
+ssh_key="${tmpdir}/bosh.pem"
 
 pushd "capi-ci-private/${BBL_STATE_DIR}"
   eval "$(bbl print-env)"
   export DIRECTOR_NAME="$(jq -e -r .bosh.directorName bbl-state.json)"
   unset BOSH_ALL_PROXY
+
+  bbl ssh-key > "${ssh_key}"
+  chmod 600 "${ssh_key}"
+  ssh_jumpbox_url=$(bbl jumpbox-address)
 popd
 
 pushd bbr-binary-release
   tar xvf *.tar
-  export BBR_BUILD_PATH=`pwd`/releases/bbr
+  export BBR_BUILD_PATH=$(pwd)/releases/bbr
 popd
 
 drats_dir="${GOPATH}/src/github.com/cloudfoundry-incubator/disaster-recovery-acceptance-tests"
 mkdir -p "${drats_dir}"
 cp -a "${drats_src}/." "${drats_dir}"
 
-env_file="${environment_dir}/metadata"
-BOSH_ENVIRONMENT="$(jq -e -r .target "${env_file}")"
-BOSH_CLIENT="$(jq -e -r .client "${env_file}")"
-BOSH_CLIENT_SECRET="$(jq -e -r .client_secret "${env_file}")"
-BOSH_CA_CERT="$(jq -e -r .ca_cert "${env_file}")"
-BOSH_GW_USER="$(jq -e -r .gw_user "${env_file}")"
-BOSH_GW_HOST="$(jq -e -r .gw_host "${env_file}")"
-BOSH_GW_PRIVATE_KEY_CONTENTS="$(jq -e -r .gw_private_key "${env_file}")"
-
-JUMPBOX_URL="$(jq -e -r .jumpbox_url "${env_file}")"
-JUMPBOX_SSH_KEY="$(jq -e -r .jumpbox_ssh_key "${env_file}")"
-JUMPBOX_USERNAME="$(jq -e -r .jumpbox_username "${env_file}")"
-
-export BOSH_ENVIRONMENT BOSH_CLIENT BOSH_CLIENT_SECRET BOSH_CA_CERT \
-       JUMPBOX_URL JUMPBOX_SSH_KEY JUMPBOX_USERNAME
-
 CF_ADMIN_PASSWORD="$(credhub get --name=/${DIRECTOR_NAME}/${CF_DEPLOYMENT_NAME}/cf_admin_password -j | jq .value -r)"
+echo ${DIRECTOR_NAME}
+echo ${CF_DEPLOYMENT_NAME}
+echo ${CF_ADMIN_PASSWORD}
 export CF_ADMIN_PASSWORD
 
-tmpdir="$( mktemp -d /tmp/run-drats.XXXXXXXXXX )"
-
-ssh_key="${tmpdir}/bosh.pem"
-echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${ssh_key}"
-chmod 600 "${ssh_key}"
-
-ssh_jumpbox_url=$(echo "${JUMPBOX_URL}" | cut -d':' -f1)
-sshuttle -e "ssh -i "${JUMPBOX_PRIVATE_KEY}" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=600" -r "${JUMPBOX_USERNAME}@${ssh_jumpbox_url}" 10.0.0.0/8 &
+sshuttle -e "ssh -i "${JUMPBOX_PRIVATE_KEY}" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=600" -r "jumpbox@${ssh_jumpbox_url}" 10.0.0.0/8 &
 tunnel_pid="$!"
 
 cleanup() {
